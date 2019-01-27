44f2811fe8fb66695bd9f51051d7eb7c27511ec4
Tyler <toreskey@gmail.com>
2019-01-27 09:35:06 -0700

    moved files aeound for better use

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,57 +1,16 @@
 import React, { Component } from 'react';
-import FacebookLogin from 'react-facebook-login';
-import { BrowserRouter } from 'react-router-dom';
+import Facebook from '../components/FacebookLogin/FacebookLogin'
+import Google from '../components/GoogleLogin/GoogleLogin'
 
 
-export default class Login extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isLoggedIn: false,
-      userID: '',
-      name: '',
-      picture: ''
-    }
-  }
-
-  componentClicked = () => console.log('clicked')
-
-  responseFacebook = response => {
-    console.log(response);
-    this.setState({
-      isLoggedIn: true,
-      userId: response.userID,
-      name: response.name,
-      picture: response.picture.data.url
-    })
-  }
-
+class Login extends Component {
   render() {
-      let fbContent;
-      if (this.state.isLoggedIn) {
-        fbContent = (
-          <div style={{
-            width: '400px',
-            margin: 'auto',
-            background: 'white'
-          }}>
-          <img src={this.state.picture} alt={this.state.name} />
-          <h2>Welcome {this.state.name}</h2>
-          </div>
-        )
-
-      }else {
-        fbContent = (
-        <FacebookLogin
-         appId="2128265563899115"
-         autoLoad={true}
-         fields="name,picture"
-         onClick={this.componentClicked}
-         callback={this.responseFacebook} />
-       )
-      }
-      return(
-        <div>{fbContent}</div>
-      )
-    }
+    return (
+      <div className="Login">
+        <Facebook />
+      </div>
+    );
+  }
 }
+
+export default Login;
