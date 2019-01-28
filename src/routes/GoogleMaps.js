cec066aeff13aef1db52a75503ea75562775e8a5
Tyler <toreskey@gmail.com>
2019-01-28 13:06:08 -0700

    user can now zoom in based off inpiut

--- src/routes/GoogleMaps.js
+++ src/routes/GoogleMaps.js
@@ -4,34 +4,81 @@ import { GoogleComponent } from 'react-google-location'
 
 const apiKey = 'AIzaSyDysvmNwccnv7MkNHYRdLkfZc7KKtHYFkQ'
 
+const params = {v: '3.exp', key: apiKey};
+
 class GoogleMaps extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      place: null,
-      lat: null,
-      lng: null
+      place: 'Colorado, USA',
+      lat: 39.5500507,
+      lng: -105.7820674,
+      zoom: 4
     };
   }
 
+  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: true,
+    });
+  }
+
+  onDragEnd(e) {
+    console.log('onDragEnd', e);
+  }
+
+  onCloseClick() {
+    console.log('onCloseClick');
+  }
+
+  onClick(e) {
+    console.log('onClick', e);
+  }
+
   render() {
-    console.log('place', this.state.place);
-    console.log('lat', this.state.lat);
-    console.log('lng', this.state.lng);
+    console.log(this.state.place);
+
     return (
-      <div className="wrapper" >
-        <GoogleComponent
-          apiKey={apiKey}
-          language={'en'}
-          country={'country:us'}
-          coordinates={true}
-          onChange={(e) => { this.setState({
-            place: e,
-            lat: e.coordinates.lat,
-            lng: e.coordinates.lng
-          })
-        }}/>
+        <div className="wrapper" >
+          <GoogleComponent
+            apiKey={apiKey}
+            language={'en'}
+            country={'country:us'}
+            coordinates={true}
+            onChange={(e) => { this.setState({
+              place: e,
+              lat: e.coordinates.lat,
+              lng: e.coordinates.lng,
+              zoom: 7
+            })
+          }}/>
+
+          <Gmaps
+            width={'800px'}
+            height={'600px'}
+            lat={this.state.lat}
+            lng={this.state.lng}
+            zoom={this.state.zoom}
+            loadingMessage={'Fishtopia'}
+            params={params}
+            onMapCreated={this.onMapCreated}>
+          <Marker
+            lat={this.state.lat}
+            lng={this.state.lng}
+            draggable={true}
+            onDragEnd={this.onDragEnd} />
+          <InfoWindow
+            lat={this.state.lat}
+            lng={this.state.lng}
+            content={'form goes here'}
+            onCloseClick={this.onCloseClick} />
+          <Circle
+            lat={this.state.lat}
+            lng={this.state.lng}
+            radius={500}
+            onClick={this.onClick} />
+        </Gmaps>
       </div>
     );
   }
